b4664b5fbbd93e597652f44028505425b5d215d2
Bogdan-Cristian Micu <bmicu2005@gmail.com>
2025-09-16 01:01:32 +0300

    modified BlackScholes.java

--- src/main/java/models/BlackScholes.java
+++ src/main/java/models/BlackScholes.java
@@ -1,4 +1,24 @@
 package models;
 
+import utils.NormalDistributionUtil;
+
 public class BlackScholes {
+
+    public double priceCall(double s, double k, double r, double sigma, double t) {
+
+        Greeks greeks = new Greeks();
+        double dOneVal = greeks.dOne(s, k, r, sigma, t);
+        double dTwoVal = greeks.dTwo(s, k, r, sigma, t);
+
+        return (s * NormalDistributionUtil.cdf(dOneVal) - k * Math.exp(-r*t) * NormalDistributionUtil.cdf(dTwoVal));
+    }
+
+    public double pricePut(double s, double k, double r, double sigma, double t) {
+
+        Greeks greeks = new Greeks();
+        double dOneVal = greeks.dOne(s, k, r, sigma, t);
+        double dTwoVal = greeks.dTwo(s, k, r, sigma, t);
+
+        return (k * Math.exp(-r*t) * NormalDistributionUtil.cdf(-dTwoVal) - s * NormalDistributionUtil.cdf(-dOneVal));
+    }
 }
