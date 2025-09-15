5d9abf801181e21d8d63c24edd72cc7daac2190a
Bogdan-Cristian Micu <bmicu2005@gmail.com>
2025-09-16 00:41:47 +0300

    modified RandomGenerator.java

--- src/main/java/utils/RandomGenerator.java
+++ src/main/java/utils/RandomGenerator.java
@@ -1,4 +1,24 @@
 package utils;
 
+import org.apache.commons.math3.random.MersenneTwister;
+
 public class RandomGenerator {
+
+    private final MersenneTwister rng;
+
+    public RandomGenerator() {
+        this.rng = new MersenneTwister();
+    }
+
+    public RandomGenerator(long seed) {
+        this.rng = new MersenneTwister(seed);
+    }
+
+    public double nextUniform(){
+        return rng.nextDouble();
+    }
+
+    public double nextGaussian(){
+        return rng.nextGaussian();
+    }
 }
